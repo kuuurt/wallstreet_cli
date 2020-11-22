57c24f0897488cb47f56ce24dc3a1574aea50792
Wenxin Ling <w.ling@mediaire.de>
2020-11-22 16:12:47 +0100

    xetra file

---
+++ wallstreet_cli/xetra.py
@@ -0,0 +1,11 @@
+def parse_csv():
+    # parse all the csvs
+    pass
+
+def download(date):
+    # download all s3 files from the given date
+    pass
+
+def get_stock_from_dataset(dataset):
+    # get stock info from dataset
+    pass
